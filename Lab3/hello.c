bdcb1cb8911a68f17fa2b9b6ec23405c8dce9845
Evgeniy Chygrynetc <evgeniy.chygrynetc@gmail.com>
2023-11-28 11:19:21 +0200

    Add files via upload

--- Lab3/hello.c
+++ Lab3/hello.c
@@ -29,79 +29,69 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <linux/module.h>
+// SPDX-License-Identifier: GPL-2-Clause
 #include <linux/init.h>
+#include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/moduleparam.h>
 #include <linux/ktime.h>
 #include <linux/slab.h>
-struct head_list
-{
-	struct head_list *next;
+
+MODULE_AUTHOR("IO-15 Chygrynetc Evheniy");
+MODULE_DESCRIPTION("Simple amount for my lab");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static uint amount = 1;
+
+module_param(amount, uint, 0444);
+MODULE_PARM_DESC(amount, "amount");
+
+struct my_struct {
+	struct list_head tlist;
 	ktime_t time;
 };
 
-MODULE_DESCRIPTION("Hello, world in Linux Kernel Training Lab3");
-MODULE_LICENSE("Dual BSD/GPL");
-
-static struct head_list *head;
-
-static int amount = 1;
-module_param(amount, uint, S_IRUGO);
-MODULE_PARM_DESC(times, "Output amount");
+static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
 {
-	uint i = 0;
 
-	struct head_list *var_1, *var_2;
-
-	head = kmalloc(sizeof(struct head_list *), GFP_KERNEL);
-
-	var_1 = head;
-	if(amount == 0)
-	{
-		printk(KERN_WARNING "Parameter == 0");
-	}
-	else if(amount >= 5 && amount <= 10)
-	{
-		printk(KERN_WARNING "5 <= Parameter <= 10.");
-	}
-	else if(amount > 10)
-	{
-		printk(KERN_ERR "Parameter > 10.");
+	if (amount == 0) {
+		printk(KERN_WARNING "Success! The parameter's 0\n");
+	} else if (amount >= 5 && amount <= 10) {
+		printk(KERN_WARNING "Success! The parameter's between 5 and 10\n");
+	} else if (amount > 10) {
+		printk(KERN_WARNING "Fail! The parameter's > 10)\n");
 		return -EINVAL;
 	}
 
+	int i;
 
-	for(i=0; i < amount; i++)
-	{
-		var_1->next = kmalloc(sizeof(struct head_list), GFP_KERNEL);
-		var_1->time = ktime_get();
-		printk(KERN_EMERG "Hello, world!\n");
-		var_2 = var_1;
-		var_1 = var_1->next;
+	for (i = 0; i < amount; i++) {
+		struct my_struct *ms = NULL;
+
+		ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+		ms->time = ktime_get();
+		list_add_tail(&ms->tlist, &my_list_head);
+
+		printk(KERN_INFO "Hello, world!\n");
 	}
-
-	if (amount != 0) {
-		kfree(var_2->next);
-		var_2->next = NULL;
-	}
-
-	printk(KERN_INFO "Amount: %d\n", amount);
 
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	struct head_list *var;
+	int count = 0;
+	struct my_struct *t, *tmp;
 
-while (head != NULL && amount != 0) {
-	var = head;
-	printk(KERN_INFO "Time: %lld\n", var->time);
-	head = var->next;
-	kfree(var);
+	list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
+		pr_info("i: %d; ktime = %lld\n", count++, ktime_to_ns(t->time));
+		list_del(&t->tlist);
+		kfree(t);
 	}
+	printk(KERN_INFO "Success! Module unloaded\n");
 }
+
 module_init(hello_init);
 module_exit(hello_exit);
